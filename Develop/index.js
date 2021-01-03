9b2fe49dee5847e2775f1bee89cd68372872a7e7
Your Name <koreysmith@Koreys-MacBook-Pro.local>
2021-01-03 14:14:49 -0700

    prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,43 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'projectDescription",
+        message: 'Project Description:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use. Include screenshots as needed.'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'List your collaborators:'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Please select a license:',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
+
+    },
+  ])
+  
+  
 // array of questions for user
 const questions = [
 
